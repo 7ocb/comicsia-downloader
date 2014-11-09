f60290cd16bcf876d72097044106ba805269d7ec
Losich Alexey <some.any.key@gmail.com>
2014-11-09 22:47:15 +0300

    Added functionality

--- comicsdl.hs
+++ comicsdl.hs
@@ -12,47 +12,44 @@ import Text.XML.Cursor (fromDocument,
                         Cursor,
                         Axis)
 
+import Debug.Trace
+import Control.Monad.Reader
+import Control.Monad (foldM)
+import Control.Monad.Except
+import Control.Applicative
 import qualified System.FilePath as P
 import Text.Printf (printf)
-import System.Environment (getArgs)    
+import System.Environment (getArgs)
+import System.Console.GetOpt
 
-import Control.Monad (liftM, liftM3)
-    
+import Safe (headMay)
+
 import qualified Data.Text as Text
 
 import Data.XML.Types (Name)
 import Text.HTML.DOM (parseLBS)
 import qualified Data.ByteString.Lazy as L
 
-maybeHead (x:_) = Just x
-maybeHead []  = Nothing
-
-get :: (Cursor -> [Text.Text]) -> Axis -> Cursor -> Maybe String
-get getter axis cursor = maybeHead $ do
-  element <- cursor $// axis
-  (Text.unpack) `fmap` (getter element)
-
-getComicsImageUrl = get (attribute "src")  (element "img" >=> hasAttribute "id")
-getNextLinkUrl    = get (attribute "href") (element "a" >=> attributeIs "id" "NextLink")
-getStripNumber    = get ($// content)      (element "div" >=> attributeIs "id" "strip-number")
-getStripTitle     = get ($// content)      (element "title")
-
 data Page = Page { stripImageUrl :: String,
                    stripNumber :: Int,
                    stripTitle :: String,
-                   nextPageUrl :: Maybe String
+                   nextPageUrl :: Maybe String,
+                   prevPageUrl :: Maybe String
                  } deriving (Show)
 
-pageFromUrl :: String -> IO (Maybe Page)
-pageFromUrl url = do
-  page <- (fromDocument . parseLBS) `fmap` (simpleHttp url)
 
-  return $ (\x -> x $ getNextLinkUrl page) `fmap` (liftM3 Page
-                                                   (getComicsImageUrl page)
-                                                   (read `fmap` (getStripNumber page))
-                                                   (getStripTitle page))
+pageFromUrl url = parsePage <$> simpleHttp url
 
-saveImage url name = simpleHttp url >>= L.writeFile name
+parsePage :: L.ByteString -> Maybe Page
+parsePage contents = Page <$> url <*> number <*> title <*> link "NextLink" <*> link "PrevLink"
+
+    where page = fromDocument $ parseLBS contents
+          get getter axis = headMay $ Text.unpack `fmap` ((page $// axis) >>= getter)
+
+          url       = get (attribute "src") (element "img" >=> hasAttribute "id")
+          link name = Just . ("http://comicsia.ru" ++) <$> get (attribute "href") (element "a" >=> attributeIs "id" name)
+          number    = read <$> get ($// content) (element "div" >=> attributeIs "id" "strip-number")
+          title     = get ($// content) (element "title")
 
 imageName page =
     printf "%04d - %s%s" (stripNumber page) safeName extension
@@ -66,27 +63,109 @@ imageName page =
           isSafe = not . (`elem` charactersBadForFs)
           charactersBadForFs = "/\\"
           maxNameLen = 120
-    
-                     
-downloadComics url = pageFromUrl url >>= guarded processPage
-    where guarded action maybeValue = case maybeValue of
-                                   Just value -> action value
-                                   Nothing -> return ()
-                                              
-          processPage  page = do
-            let fileName = (imageName page)
-                           
-            saveImage (stripImageUrl page) fileName
 
-            putStrLn $ "Wrote " ++ fileName
-            
-            guarded downloadComics $ ("http://comicsia.ru" ++ ) `fmap` (nextPageUrl page)
+saveComics page = liftIO $ do
+  let fileName = (imageName page)
+
+  simpleHttp (stripImageUrl page) >>= L.writeFile fileName
+
+  putStrLn $ "Wrote " ++ fileName
+
+goDownload nextPage page =
+  case (nextPage page) of
+    Just url -> pageFromUrl url >>= maybe (return ()) process
+
+    Nothing -> return ()
+
+  where process next = do
+          cfg <- ask
+          if inRange cfg next
+          then do saveComics next
+                  goDownload nextPage next
+          else return ()
+
+inRange :: Cfg -> Page -> Bool
+inRange cfg p = not $ (aboveRange cfg p) || (underRange cfg p)
+
+aboveRange :: Cfg -> Page -> Bool
+aboveRange cfg p = maybe False ((stripNumber p) >) $ cfgLimit cfg
+
+underRange :: Cfg -> Page -> Bool
+underRange cfg p =  maybe False ((stripNumber p) <) $ cfgFrom cfg
+
+data Opt = Start String
+         | Limit String
+         | Help
+           deriving (Show, Eq)
+
+arguments = [ (Option ['f'] ["first"] (ReqArg Start "FIRST") "Number of strip to start from")
+            , (Option ['l'] ["last"] (ReqArg Limit "LAST") "Number of strip to end on, inclusive")
+            , (Option ['h'] ["help"]  (NoArg Help) "Show this help")
+            ]
+
+data Cfg = Cfg {
+      cfgFrom :: Maybe Int,
+      cfgLimit :: Maybe Int
+    } deriving (Show, Eq)
+
+parseArgs :: [String] -> Except String (Cfg, String)
+parseArgs args = do
+  let (opts, urls, errors) = getOpt Permute arguments args
+      guard cond err = when cond $ throwError err
+
+      asInt :: String -> Except String Int
+      asInt s = do let r = reads s :: [(Int, String)]
+                   guard (null r) $ "Can't parse " ++ s ++ " as int"
+                   let ((i,_):_) = r
+                   return i
+
+      process :: Cfg -> Opt -> Except String Cfg
+      process c (Start s) = asInt s >>= \i -> return $ c { cfgFrom = Just i }
+      process c (Limit s) = asInt s >>= \i -> return $ c { cfgLimit = Just i }
+
+  guard (Help `elem` opts) $ usageInfo "comicsdl [opts] url" arguments
+  guard (not $ null errors) (head errors)
+  guard (null urls) "No urls provided"
+
+  cfg <- foldM process (Cfg Nothing Nothing) opts
+
+  guard ((Just True) == ((>) <$> (cfgFrom cfg) <*> (cfgLimit cfg))) "From can't be after limit"
+
+  return (cfg, head urls)
+
+type App = ReaderT Cfg IO
+
+app :: App ()
+app = liftIO $ putStrLn "app"
+
+locatePage :: String -> App ()
+locatePage url = do
+  p <- pageFromUrl url
+  cfg <- ask
+
+  case p of
+    Just page -> do
+           let go next = do
+                   liftIO $ putStrLn $ "skipping strip: " ++ (show $ stripNumber page)
+                   maybe (return ()) locatePage $ next page
+               goDl next = goDownload next page
 
 
-                 
+
+           if inRange cfg page
+           then do saveComics page
+                   goDl nextPageUrl
+                   goDl prevPageUrl
+           else if underRange cfg page
+                then go nextPageUrl
+                else go prevPageUrl
+
+    Nothing -> return ()
+
 main = do
-  args <- getArgs
+  args <- (runExcept . parseArgs) <$> getArgs
 
-  if null args
-  then putStrLn "Usage: comicsdl <url-to-start-from>"
-  else downloadComics $ head args 
+  case args of
+    Left error -> putStrLn error
+
+    Right (cfg, url) -> runReaderT (locatePage url) cfg
